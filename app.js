310f4ddc02c37da3c3e43e1fef7f0b5a3d5468e1
Germán Cutraro <germancutraro@hotmail.com>
2017-05-25 04:58:21 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+class Cart {
+  constructor (article) {
+    this.article = article;
+    this.items = {};
+  }
+  addItem (article, count) {
+    this.items[article] = count;
+  }
+  removeItem (article) {
+    delete this.items[article];
+  }
+  seeItems () {
+    for(let k in this.items) {
+      console.log(`${k} | ${this.items[k]}`);
+    }
+  }
+}
+
+const books = new Cart('Philosophy Books');
+console.log(`Items of ${books.article}`);
+books.addItem('Metaphysics of Aristotle', 1);
+books.addItem('Plato', 2);
+books.addItem('Socrates', 1);
+books.addItem('Edison', 300);
+books.removeItem('Edison');
+books.seeItems();
